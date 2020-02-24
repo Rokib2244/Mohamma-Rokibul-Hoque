bc9d97b9b90f09f55e20d9261f106b431b51459c
Rokib2244 <45006362+Rokib2244@users.noreply.github.com>
2020-02-24 20:32:07 +0600

    Revert "DiagnosticCebter Management"
    This reverts commit c2631f4cf68b8e9901fbc15c24a7f844c8be4115.

--- Bitm Projcect Diagnostic/DiagonasticCenterManagementUpdateApp/DiagonasticCenterManagementApp/UI/js/hideSaveButton.js
+++
@@ -1,7 +0,0 @@
-﻿$(document).ready(function() {
-    var tables = document.getElementsByTagName("table");
-    if (tables.length === 0) {
-        console.log("total Hidden");
-        $("#totalAndSaveDiv").hide();
-    }
-});
--- Bitm Projcect Diagnostic/DiagonasticCenterManagementUpdateApp/DiagonasticCenterManagementApp/UI/js/script.js
+++
@@ -1,7 +0,0 @@
-﻿$('.header-item').hover(function(event) {
-    $(this).children('ul').slideDown(400, function() {
-
-    }).css('display', 'block');
-}, function() {
-    $(this).children('ul').slideUp(400);
-});
